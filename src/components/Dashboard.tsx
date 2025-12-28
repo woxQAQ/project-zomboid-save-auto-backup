38bb51d9d9af2eb005c375a5a8969fb87ae6f794
woxQAQ <woxqaq@gmail.com>
2025-12-28 23:49:41 +0800

    style(Dashboard): 为备份列表添加滚动条以改善用户体验

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -282,7 +282,7 @@ export const Dashboard: React.FC = () => {
       <AutoBackupControl selectedSave={selectedSave} showToast={showToast} />
 
       {/* Backup List */}
-      <div className="flex-1 min-h-0">
+      <div className="flex-1 min-h-0 overflow-y-auto">
         <BackupList
           key={refreshKey}
           saveName={selectedSave}
